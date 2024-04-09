52737e9cf7c63c814a4fb72f1c3a72c5ca6f6526
aguedaaltaria <156155201+aguedaaltaria@users.noreply.github.com>
2024-04-09 15:54:50 -0700

    Switch the switch
    I replace the switch case function with the dictionary function with the mathematical signs

--- calculadora.js
+++ calculadora.js
@@ -11,202 +11,37 @@ function borrar() {
     let salida = document.getElementById("letraGrande").innerHTML;
     let arrSalida = [...salida]
     let a = arrSalida.pop();
-    // let stiSalida = arrSalida.toString();
-    // let nuevaSalida = stiSalida.replace(/,/g, "");
     arrSalida = arrSalida.toString().replace(/,/g, "");
-    
     document.getElementById("letraGrande").innerHTML = arrSalida;
 } 
 
-//////////numeros////////////////
+////////// Numeros ////////////////
 
 function numeros(value) {
     let salida = document.getElementById("letraGrande");
     salida.innerHTML += value;
-    // salida.style.color = "negro";
 }
 
 function signoEscrito(value) {
     let salida = document.getElementById("letraGrande");
     salida.innerHTML += value;
-    // salida.style.color = "#7c6897"
 }
 
-/////////Numero antiguos/////////
-
-// function uno() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "1";
-// }
-
-// function dos() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "2";
-// }
-
-// function tres() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "3";
-// }
-
-// function cuatro() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "4";
-// }
-
-// function cinco() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "5";
-// }
-
-// function seis() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "6";
-// }
-
-// function siete() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "7";
-// }
-
-// function ocho() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "8";
-// }
-
-// function nueve() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "9";
-// }
-
-// function cero() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "0";
-// }
+////////// Punto //////////////////
 
 function punto() {
     let salida = document.getElementById("letraGrande");
     salida.innerHTML += ".";
 }
 
-///////////signo//////////////
-
-// function signoSuma() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "+";
-// }
-
-// function signoResta() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "-";
-// }
-
-// function signoMultiplicacion() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "x";
-// }
-
-// function signoDivicion() {
-//     let salida = document.getElementById("letraGrande");
-//     salida.innerHTML += "÷";
-// }
+/////////// Promedio //////////////
 
 function signoPromedio() {
     let salida = document.getElementById("letraGrande");
     salida.innerHTML += "%";
 }
 
-//////////////procedimientos//////////////////
-
-// function sumar() {
-//     let salida = document.getElementById("letraGrande").innerHTML;
-//     let a = 0;
-//     let b = salida.indexOf("+");
-//     let num1sti = salida.slice(a, b);
-//     let num1 = parseFloat(num1sti);
-//     let c = salida.indexOf("+");
-//     let d = salida.length;
-//     let num2sti = salida.slice(c + 1, d);
-//     let num2 = parseFloat(num2sti);
-//     return num1 + num2
-// }
-
-// function restar() {
-//     let salida = document.getElementById("letraGrande").innerHTML;
-//     let a = 0;
-//     let b = salida.indexOf("-");
-//     let num1sti = salida.slice(a, b);
-//     let num1 = parseFloat(num1sti);
-//     let c = salida.indexOf("-");
-//     let d = salida.length;
-//     let num2sti = salida.slice(c + 1, d);
-//     let num2 = parseFloat(num2sti);
-//     return num1 - num2
-// }
-
-// function multiplicar() {
-//     let salida = document.getElementById("letraGrande").innerHTML;
-//     let a = 0;
-//     let b = salida.indexOf("x");
-//     let num1sti = salida.slice(a, b);
-//     let num1 = parseFloat(num1sti);
-//     let c = salida.indexOf("x");
-//     let d = salida.length;
-//     let num2sti = salida.slice(c + 1, d);
-//     let num2 = parseFloat(num2sti);
-//     return num1 * num2
-// }
-
-// function dividir() {
-//     let salida = document.getElementById("letraGrande").innerHTML;
-//     let a = 0;
-//     let b = salida.indexOf("÷");
-//     let num1sti = salida.slice(a, b);
-//     let num1 = parseFloat(num1sti);
-//     let c = salida.indexOf("÷");
-//     let d = salida.length;
-//     let num2sti = salida.slice(c + 1, d);
-//     let num2 = parseFloat(num2sti);
-//     return num1 / num2
-// }
-
-// function porciento() {
-//     let salida = document.getElementById("letraGrande").innerHTML;
-//     let a = 0;
-//     let b = salida.indexOf("%");
-//     let num1sti = salida.slice(a, b);
-//     let num1 = parseFloat(num1sti);
-//     return (num1 / 100)
-// }
-
-// ///////////////resultado////////////
-
-// function procedimiento() {
-//     let salida = document.getElementById("letraGrande").innerHTML;
-
-// console.log(salida);
-//     let resultado = "";
-//     if (salida.includes("+") == true) {
-//         resultado = sumar()
-//     } 
-//     if (salida.includes("-") == true) {
-//         resultado = restar()
-//     } 
-//     if (salida.includes("x") == true) {
-//         resultado = multiplicar()
-//     } 
-//     if (salida.includes("÷") == true) {
-//         resultado = dividir()
-//     } 
-//     if (salida.includes("%") == true) {
-//         resultado = porciento()
-//     } 
-
-// console.log(resultado);
-//     document.getElementById("letraPequenia").innerHTML = resultado;
-// }
-
-// Resultado 2 ///////////////// Ahora se pueden hacer mas de 1 procedimiento a la vez
+////////// Resultado = /////////////
 
 const signos = ["+", "-", "x", "÷"];
 
@@ -220,23 +55,16 @@ function procedimiento() {
     let filSigno = a.filter(b => signos.includes(b));
     
     let resultado = nums[0];
+    const procesos = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        'x': (a, b) => a * b,
+        '÷': (a, b) => a / b
+    }
 
     for (let i = 1; nums.length > i; i++) {
         let usoSigno = filSigno[i - 1];
-        switch (usoSigno) {
-            case "+":
-                resultado = resultado + nums[i];
-                break;
-            case "-":
-                resultado = resultado - nums[i];
-                break;
-            case "x":
-                resultado = resultado * nums[i];
-                break;
-            case "÷":
-                resultado = resultado / nums[i];
-                break;
-        }
+        resultado = procesos[usoSigno](resultado, nums[i]);
     }
     document.getElementById("letraPequenia").innerHTML = resultado;
 }
